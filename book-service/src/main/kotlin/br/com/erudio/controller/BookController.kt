4eb362b30f981ae4928368a44e9340e951037819
Ingrid Portilho <ingrid.portilho23@gmail.com>
2025-03-19 21:27:59 -0300

    Microsserviços.

---
+++ book-service/src/main/kotlin/br/com/erudio/controller/BookController.kt
@@ -0,0 +1,77 @@
+package br.com.erudio.controller
+
+import br.com.erudio.model.Book
+import br.com.erudio.model.Cambio
+import br.com.erudio.proxy.CambioProxy
+import br.com.erudio.repository.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
+
+@RestController
+@RequestMapping("book-service")
+class BookController {
+
+    @Autowired
+    private lateinit var environment: Environment
+
+    @Autowired
+    private lateinit var repository: BookRepository
+
+    @Autowired
+    private lateinit var proxy: CambioProxy
+
+    @GetMapping(value = ["/{id}/{currency}"])
+    fun findBook(
+        @PathVariable("id") id: Long,
+        @PathVariable("currency") currency: String
+    ): Book? {
+        val book = repository.findById(id)
+            .orElseThrow{RuntimeException("Book Not found")}
+
+        val cambio = proxy.getCambio(book.price, "USD", currency)
+
+        val port = environment.getProperty("local.server.port")
+        book.environment = "BOOK PORT $port CAMBIO PORT ${cambio!!.environment}"
+        book.currency = currency
+        book.price = cambio!!.convertedValue
+
+        return book
+    }
+
+    @GetMapping(value = ["/v1/{id}/{currency}"])
+    fun findBookV1(
+        @PathVariable("id") id: Long,
+        @PathVariable("currency") currency: String
+    ): Book? {
+        val book = repository.findById(id)
+            .orElseThrow{RuntimeException("Book Not found")}
+
+        val params = HashMap<String, String>()
+
+        params["amount"] = book.price.toString()
+        params["from"] = "USD"
+        params["to"] = currency
+
+
+        val response = RestTemplate()
+            .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio::class.java,
+                params
+            )
+
+        val cambio = response.body
+
+        val port = environment.getProperty("local.server.port")
+        book.environment = port
+        book.currency = currency
+        book.price = cambio!!.convertedValue
+
+        return book
+    }
+}
---
+++ book-service/src/main/kotlin/br/com/erudio/model/Book.kt
@@ -0,0 +1,32 @@
+package br.com.erudio.model
+
+import jakarta.persistence.*
+import java.util.Date
+
+@Entity(name = "book")
+
+data class Book (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(name = "author", nullable = false, length = 180)
+    var author: String = "",
+
+    @Column(name = "title", nullable = false, length = 250)
+    var title: String = "",
+
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    var lanchDate: Date? = null,
+
+    @Column(name = "price", nullable = false)
+    var price: Double? = null,
+
+    @Transient
+    var currency: String = "",
+
+    @Transient
+    var environment: String? = ""
+)
